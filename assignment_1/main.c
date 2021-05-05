90a088e0625c0749f4c48bc0ceea99d0da09c359
PatThePhysicist <patricia.golaszewska@mail.utoronto.ca>
2021-05-05 19:04:59 -0400

    Updated the C file

--- assignment_1/main.c
+++ assignment_1/main.c
@@ -9,5 +9,29 @@ int main() {
 
     printf("The average of 1, 2, 3 and 4 is: %.4f\n", result);
     return 0;    
+
+print("Patricia was here! HAHAHAHAHAHA I DID IT!")
+
+print(" 
+		According to all known laws
+		of aviation,
+		
+		  
+		there is no way a bee
+		should be able to fly.
+		
+		  
+		Its wings are too small to get
+		its fat little body off the ground.
+		
+		  
+		The bee, of course, flies anyway
+		
+		  
+		because bees don't care
+		what humans think is impossible")	
+
+
+
 }
 
